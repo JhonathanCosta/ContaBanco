4dbce88f4cc532a6e12d6917b0bff7843e7fa2ab
jhonathanCosta <jhonathansouzacosta@gmail.com>
2023-09-11 20:19:56 -0300

    Cadastro de Clientes

---
+++ ContaTerminal.java
@@ -0,0 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+            System.out.println("Digite sua conta");
+            int conta = scanner.nextInt();
+
+            System.out.println("Digite sua agência");
+            String agencia = scanner.next();
+
+            System.out.println("Digite seu nome");
+            String nome = scanner.next();
+            
+            System.out.println("Digite seu saldo");
+            double saldo = scanner.nextDouble();
+
+            System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo R$" + saldo + " já está disponível para saque");
+        } 
+    }
+}
